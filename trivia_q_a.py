603307b1526f13556c27865b247ed5e1872d5b45
matthewculaton <83097393+matthewculaton@users.noreply.github.com>
2021-04-23 15:46:30 -0700

    Update trivia_q_a.py

--- trivia_q_a.py
+++ trivia_q_a.py
@@ -2,3 +2,9 @@ trivia_dictionary = {
     "What is the capital of California?":"Sacramento",
     "What year was SJSU founded":"1857"
 }
+
+def trivia():
+    q_pair = key, val = random.choice(list(trivia_dictionary.items()))
+    ques = q_pair[0]
+    answer = q_pair[1]
+    return ques
